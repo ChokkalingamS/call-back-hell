1bcc9f7929da53ee631a4af5bf4429a1b3aea1d7
Chokkalingam S <91081529+ChokkalingamS@users.noreply.github.com>
2021-10-11 22:54:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let p=document.querySelector('.countdown');
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+setTimeout(function() {
+ p.innerHTML="Happy Independence Day";
+},1000);
+     p.innerHTML=1;
+},1000);
+     p.innerHTML=2;
+},1000);
+     p.innerHTML=3;
+},1000);
+     p.innerHTML=4;
+},1000);
+      p.innerHTML=5;
+},1000);
+    p.innerHTML=6;
+},1000);
+    p.innerHTML=7;
+},1000);
+    p.innerHTML=8;
+},1000);
+  p.innerHTML=9;
+},1000);
+    p.innerHTML=10;
+}, 1000);
+
